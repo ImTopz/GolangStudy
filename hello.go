89e0cbbd56a0506aae3cbdeda7dfde3bc6de2f41
ImTopz <1976521709@qq.com>
2023-01-01 20:50:22 +0800

    初始化

--- hello.go
+++ hello.go
@@ -8,4 +8,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("你好，这是你的第一行golang代码！")
+	fmt.Println("go语言有很多的特性")
+	fmt.Println("你可以通过go run hello.go来运行该代码")
+	fmt.Println("也可以通过 go build hello.go来生成一下二进制文件")
 }
