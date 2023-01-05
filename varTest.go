e56aa91b0e408c7f12499793ab042282a05f0769
ImTopz <1976521709@qq.com>
2023-01-05 20:27:37 +0800

    new

---
+++ varTest.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	//var flag = false
+	//var number float64
+	//number = 3.1415592622222332122332122332122
+	//f := "Runoob"
+	//fmt.Println(flag)
+	//fmt.Println(number)
+	//fmt.Println(f)
+	var sum int
+	for i := 0; i <= 100000000; i++ {
+		sum += i
+	}
+}
---
+++ webTest.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func httpGet() {
+	resp, err := http.Get("http://www.baidu.com")
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
+}
+
+func main() {
+	httpGet()
+}
